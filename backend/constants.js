2d7fb91c0702a1a06498e7d73decc744986653ab
ZeberMVP <rubeen722@gmail.com>
2023-04-19 16:05:41 +0200

    first commit

---
+++ backend/constants.js
@@ -0,0 +1,5 @@
+// Replace the value with your NFT contract address
+const CRYPTODEVS_NFT_CONTRACT_ADDRESS =
+    "0x0Aa0F7C4AAd6731A92e9855C4b697cD24FB140b1";
+
+module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
